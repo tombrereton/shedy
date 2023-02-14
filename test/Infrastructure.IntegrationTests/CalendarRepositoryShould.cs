df7e36eda1ffd39c1a18c7c13023cc9dd83b7c38
tombrereton <tbrereton89@gmail.com>
2023-02-14 16:06:14 +1100

    Add save calendar to repo int test

--- test/Infrastructure.IntegrationTests/CalendarRepositoryShould.cs
+++ test/Infrastructure.IntegrationTests/CalendarRepositoryShould.cs
@@ -1,37 +1,88 @@
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
 using Shedy.Core.Builders;
-using Shedy.Core.Calendar;
+using Shedy.Core.Interfaces;
 using Shedy.Infrastructure.Database;
 
 #pragma warning disable CS0618
 
 namespace Shedy.Infrastructure.IntegrationTests;
 
-public sealed class PostgreSqlTest : IAsyncLifetime
+public sealed class CalendarRepository : IAsyncLifetime
 {
     private readonly TestcontainerDatabase _postgresqlContainer = new ContainerBuilder<PostgreSqlTestcontainer>()
         .WithDatabase(new PostgreSqlTestcontainerConfiguration
         {
-            Database = "db",
-            Username = "postgres",
+            Database = "test",
+            Username = "shedy",
             Password = "postgres",
         })
         .Build();
 
-    private readonly ServiceProvider _services;
+    private ServiceProvider _services = null!;
 
-    public PostgreSqlTest()
+    [Fact]
+    public async Task GetCalendar()
     {
-        _postgresqlContainer.StartAsync().GetAwaiter().GetResult();
+        // arrange
+        var db = _services.GetRequiredService<ShedyDbContext>();
+        var calendar = new CalendarBuilder()
+            .CreateCalendar()
+            .WithNewCalendarId()
+            .WithUserId(Guid.NewGuid())
+            .WithDefaultOpeningHours()
+            .Build();
+        await db.Calendars.AddAsync(calendar);
+        await db.SaveChangesAsync();
+
+        var repo = _services.GetRequiredService<ICalendarRepository>();
+
+        // act
+        var result = await repo.GetAsync(calendar.Id, default);
+
+        // assert
+        result.OpeningHours.Should().Equal(calendar.OpeningHours);
+        result.UserId.Should().Be(calendar.UserId);
+    }
+
+    [Fact]
+    public async Task SaveCalendar()
+    {
+        // arrange
+        var db = _services.GetRequiredService<ShedyDbContext>();
+        var calendar = new CalendarBuilder()
+            .CreateCalendar()
+            .WithNewCalendarId()
+            .WithUserId(Guid.NewGuid())
+            .WithDefaultOpeningHours()
+            .Build();
+
+        var repo = _services.GetRequiredService<ICalendarRepository>();
+
+        // act
+        await repo.SaveAsync(calendar, default);
+
+        // assert
+        var result = await db.Calendars.FirstOrDefaultAsync(x => x.Id == calendar.Id);
+        result.Should().NotBeNull();
+        result!.OpeningHours.Should().Equal(calendar.OpeningHours);
+        result.UserId.Should().Be(calendar.UserId);
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgresqlContainer.StartAsync();
+
+        var keyValuePairs = new KeyValuePair<string, string?>[]
+            { new("ConnectionStrings:Database", _postgresqlContainer.ConnectionString) };
+
         var config = new ConfigurationBuilder()
-            .AddInMemoryCollection(new KeyValuePair<string, string?>[]
-                { new("ConnectionStrings:Database", _postgresqlContainer.ConnectionString) })
+            .AddInMemoryCollection(keyValuePairs)
             .Build();
 
         _services = new ServiceCollection()
@@ -39,26 +90,7 @@ public sealed class PostgreSqlTest : IAsyncLifetime
             .BuildServiceProvider();
 
         var db = _services.GetRequiredService<ShedyDbContext>();
-        db.Database.EnsureCreated();
-    }
-
-
-    [Fact]
-    public async Task ExecuteCommand()
-    {
-        var db = _services.GetRequiredService<ShedyDbContext>();
-        var calendar = new CalendarBuilder()
-            .CreateCalendar()
-            .WithNewCalendarId()
-            .WithDefaultOpeningHours()
-            .Build();
-        await db.Calendars.AddAsync(calendar);
-        await db.SaveChangesAsync();
-
-    }
-
-    public async Task InitializeAsync()
-    {
+        await db.Database.EnsureCreatedAsync();
     }
 
     public async Task DisposeAsync()
